72aa9088251b939995cd87185201cc068b8fbade
sanjaymagar <sanjay.magar@zakipoint.com>
2019-04-06 00:31:17 +0545

    D3js has been integrated and make area chart and line chart

---
+++ src/components/DrawChart.vue
@@ -0,0 +1,40 @@
+<template>
+    
+</template>
+
+
+<script>
+import * as d3 from 'd3';
+const data = [99, 71, 78, 25, 36, 92];
+export default {
+  name: 'non-vue-line-chart',
+  template: '<div></div>',
+  mounted() {
+    const svg = d3.select(this.$el)
+      .append('svg')
+      .attr('width', 500)
+      .attr('height', 270)
+      .append('g')
+      .attr('transform', 'translate(0, 10)');
+    const x = d3.scaleLinear().range([0, 430]);
+    const y = d3.scaleLinear().range([210, 0]);
+    d3.axisLeft().scale(x);
+    d3.axisTop().scale(y);
+    x.domain(d3.extent(data, (d, i) => i));
+    y.domain([0, d3.max(data, d => d)]);
+    const createPath = d3.line()
+      .x((d, i) => x(i))
+      .y(d => y(d));
+    svg
+        .append('path')
+        .attr("fill", "none")
+        .attr("stroke", "#76BF8A")
+        .attr("stroke-width", 3)
+        .attr('d', createPath(data));
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,13 +2,69 @@
     <div class="about">
         <h3>About Section</h3>
         <p>This is vuejs crash course by Brad Traversy</p>
+        <h1>Area Chart</h1>
+        <DrawAreaChart
+        @select="onSelect"
+        :data="data"
+        :ceil="max"
+        class="area-chart" />
+        <div class="content">
+        <h3>Selected Value: {{currentValue}}</h3>
+        <div>
+            <label>Record Count: </label>
+            <input v-model:value="itemCount" />
+        </div>
+        <div>
+            <label>Min Value: </label>
+            <input v-model:value="min" />
+        </div>
+        <div>
+            <label>Max Value: </label>
+            <input v-model:value="max" />
+        </div>
+        </div>
+
+        <DrawChart/>
+        <DrawLineChart/>
+
     </div>
 </template>
 
 <script>
+import DrawChart from '../components/DrawChart'
+import DrawLineChart from '../components/DrawLineChart'
+import DrawAreaChart from '../components/DrawAreaChart'
+import AreaData from '../data.js'
 export default {
-
-}
+   
+    data() {
+    return {
+      data: [],
+      chartWidth: 0,
+      currentValue: null,
+      itemCount: 25,
+      min: 10,
+      max: 100,
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      this.data = AreaData(this.itemCount,
+        parseInt(this.min, 10),
+        parseInt(this.max, 10));
+    }, 2000);
+  },
+  methods: {
+    onSelect(value) {
+      this.currentValue = value;
+    },
+  },
+   components: {
+        DrawChart,
+        DrawLineChart,
+        DrawAreaChart
+    }
+};
 </script>
 
 <style scoped>
